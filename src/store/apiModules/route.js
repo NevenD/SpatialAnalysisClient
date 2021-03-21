e18e15e7a504b2f0d8445cecf1d6cbed685c610e
Neven Dimač <neven.dimac@gmail.com>
2021-03-21 20:35:00 +0100

    added route saving properties

--- src/store/apiModules/route.js
+++ src/store/apiModules/route.js
@@ -4,15 +4,36 @@ const _openRouteRepository = Repository.get("IRouteRepository");
 export default {
   state: {
     _POST_RETURN_MSG: null,
+    _SAVE_ROUTE_COORDINATES: null,
+    _SAVE_ROUTE_DURATION: null,
+    _SAVE_ROUTE_LENGHT: null,
   },
   getters: {
     _POST_RETURN_MSG_: (state) => {
       return state._POST_RETURN_MSG;
     },
+    _SAVE_ROUTE_COORDINATES_: (state) => {
+      return state._SAVE_ROUTE_COORDINATES;
+    },
+    _SAVE_ROUTE_DURATION_: (state) => {
+      return state._SAVE_ROUTE_DURATION;
+    },
+    _SAVE_ROUTE_LENGHT_: (state) => {
+      return state._SAVE_ROUTE_LENGHT;
+    },
   },
   mutations: {
     SET_ASYNC_POST_RETURN_MSG(state, data) {
       state._POST_RETURN_MSG = data;
+    },
+    SAVE_ROUTE_COORDINATES(state, data) {
+      state._SAVE_ROUTE_COORDINATES = data;
+    },
+    SAVE_ROUTE_DURATION(state, data) {
+      state._SAVE_ROUTE_DURATION = data;
+    },
+    SAVE_ROUTE_LENGHT(state, data) {
+      state._SAVE_ROUTE_LENGHT = data;
     },
   },
   actions: {
@@ -28,6 +49,18 @@ export default {
           commit("SET_ASYNC_POST_RETURN_MSG", statusRoute);
         }
       });
+    },
+    _SAVE_ROUTE_COORDINATES_({ commit }, payload) {
+      commit("SAVE_ROUTE_COORDINATES", payload);
+    },
+    _SAVE_ROUTE_COORDINATES_({ commit }, payload) {
+      commit("SAVE_ROUTE_COORDINATES", payload);
+    },
+    _SAVE_ROUTE_DURATION_({ commit }, payload) {
+      commit("SAVE_ROUTE_DURATION", payload);
+    },
+    _SAVE_ROUTE_LENGHT_({ commit }, payload) {
+      commit("SAVE_ROUTE_LENGHT", payload);
     },
   },
 };
